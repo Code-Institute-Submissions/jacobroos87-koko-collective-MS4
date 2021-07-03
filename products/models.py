2672870073e4d5e958b2f7f22e50283c09f81acb
jacobroos87 <jacobroos87@gmail.com>
2021-07-03 14:26:20 +0000

    Added products to products template and also featured items to home page

--- products/models.py
+++ products/models.py
@@ -35,5 +35,5 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='media/')
+    image = models.ImageField()
     default = models.BooleanField(default=False)

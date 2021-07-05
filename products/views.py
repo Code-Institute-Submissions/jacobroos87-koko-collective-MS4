e4d81132fd7a525322140510a43843dddad59d26
jacobroos87 <jacobroos87@gmail.com>
2021-07-05 16:55:10 +0000

    Made some changes to the navbar and added shop category functionality

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404, reverse
-from .models import Product
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from .models import Product, Category
 
 # Create your views here.
 
@@ -9,6 +9,12 @@ from .models import Product
 def all_products(request):
     products = Product.objects.all()
     query = None
+    categories = None
+
+    if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     if 'q' in request.GET:
             query = request.GET['q']
@@ -24,6 +30,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)

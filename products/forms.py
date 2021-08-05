5d71ed10753e34df24a0c1aca6f137ab8d63399f
jacobroos87 <jacobroos87@gmail.com>
2021-08-05 10:50:49 +0000

    Added images to edit product and edited wishlist to include buttons for smaller devices

--- products/forms.py
+++ products/forms.py
@@ -9,25 +9,24 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ['category', 'sku', 'name', 'description', 'has_sizes', 'price', 'avg_rating', 'is_featured', 'main_image']
 
+    main_image = forms.ImageField(label='Main Image', required=True, widget=CustomClearableFileInput)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
-        self.fields['main_image'].required = True
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+            field.widget.attrs['class'] = 'rounded-0'
 
 
 class ImageForm(ProductForm):
 
-    images = forms.FileField(
-        widget=forms.ClearableFileInput(attrs={'multiple': True}))
-
     class Meta(ProductForm.Meta):
         fields = ProductForm.Meta.fields + ['images',]
-        
+
+    images = forms.FileField(label='Extra Images', required=False, widget=CustomClearableFileInput(attrs={'multiple': True}))
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['images'].required = False

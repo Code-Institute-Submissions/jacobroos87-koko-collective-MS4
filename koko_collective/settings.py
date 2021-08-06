7ab1256dfb305ad551eb296c8af104fff8677a59
jacobroos87 <jacobroos87@gmail.com>
2021-08-06 11:09:52 +0000

    Added media context_processors to templates

--- koko_collective/settings.py
+++ koko_collective/settings.py
@@ -86,6 +86,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
                 'wishlist.contexts.wishlist_items',
             ],
@@ -206,7 +207,7 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-    #ADDED BY JO FOR TESTING 
+    # ADDED BY JO FOR TESTING
     print("Updating MEDIA_URL to ", MEDIA_URL)
 # Stripe
 
